e5b209a3b25a1d9e42e37f29475d46eeab666d7a
etiya <john19982011@gmail.com>
2020-11-18 14:11:52 +0200

    finish

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {User} from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor(private http: HttpClient) {
+  }
+  getData(): Observable<any> {
+    return this.http.get('http://localhost:3000/profiles');
+  }
+  passData(formValue: object): Observable<object> {
+    return this.http.post('http://localhost:3000/profiles', formValue);
+  }
+  deleteUser(i: number): Observable<any> {
+    return this.http.delete(`http://localhost:3000/profiles/${i}`);
+  }
+}
